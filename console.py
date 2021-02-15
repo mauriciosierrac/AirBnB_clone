b470e5f3b54061bbcee17fb1bcf61b64fd7063a5
mauricio.sierrac <2376@holbertonschool.com>
2021-02-15 17:29:24 +0000

    update console :snake:

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import shlex
 from models.base_model import BaseModel
 from models import storage
 
+
 class HBNBCommand(cmd.Cmd):
     """class console command"""
     prompt = "(hbnb) "
@@ -34,11 +35,77 @@ class HBNBCommand(cmd.Cmd):
                 print("** class name missing **")
         else:
             print("** class doesn't exist **")
-    
+
     def do_show(self, line):
+        '''print the string representation of an instance based on the class name'''
+        arg = line.split()
+        odic = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.class_list:
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        else:
+            odic = storage.all()
+            for key, value in odic.items():
+                if value.id == arg[1] and value.__class__.__name__ == arg[0]:
+                    print(value.__str__())
+                    return
+            print("** no instance found **")
+
+    def do_destroy(self, line):
+        '''Deletes an instance based on the class name and id'''
+        arg = line.split()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in self.class_list:
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
+        else:
+            odic = storage.all()
+            for key, value in odic.items():
+                if value.id == arg[1] and value.__class__.__name__ == arg[0]:
+                    del(odic[key])
+                    models.storage.save()
+                    return
+            print("** no instance found **")
+
+    def do_all(self, line):
+        ''' Prints all string representation of all instances based or not on the class name'''
+        arg = line.split()
+        if len(arg) > 0 and arg[0] not in self.class_list:
+            print("** class doesn't exist **")
+        else:
+            ndic = []
+            odic = storage.all()
+            for values in odic.values():
+                if len(arg) > 0 and arg[0] == values.__class__.__name__:
+                    ndic.append(values.__str__())
+                elif len(arg) == 0:
+                    ndic.append(values.__str__())
+            print(ndic)
+            
+    def do_update(self, line):
+        ''' Updates an instance based on the class name and id by adding or updating attribute '''
+        arg = line.split()
+        odic = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        if arg[0] not in self.class_list:
+            print("** class doesn't exist **")
+        if len(arg) < 2:
+            print("** instance id missing **")
+        if "{}.{}".format(arg[0], arg[1]) not in odic.keys():
+            print("** no instance found **")
+        if len(arg) < 3:
+            print("** attribute name missing **")
+        if len(arg) < 4:
+            print("** value missing **")
         
-
-
+            
+            
 
 
 if __name__ == '__main__':
